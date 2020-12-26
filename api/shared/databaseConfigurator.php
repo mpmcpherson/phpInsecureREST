9feb76cff1b7310d0a2cf6df18cce54a9a52e40c
Michael McPherson <michael@mpmcpherson.com>
2020-12-26 02:07:33 -0500

    added manager file and started stubbing in functions

---
+++ api/shared/databaseConfigurator.php
@@ -0,0 +1,15 @@
+<?php
+namespace REST_API;
+require_once 'CouchDBConnection.php';
+require_once 'CouchDBRequest.php';
+require_once 'CouchDBResponse.php';
+require_once 'genericException.php';
+require_once 'abstractRestConnection.php';
+
+	class databaseConfigurator extends databaseManager{
+
+		function __construct(){}
+		function getUserId(){}
+	}
+
+?>

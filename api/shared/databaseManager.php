03a566453d45d127f46cc12c5f61e570d3eca91f
Michael McPherson <michael@mpmcpherson.com>
2020-12-26 01:52:01 -0500

    tweaked my handleReturns to not be stupid about assigning the Key=>Value in situations where the Value is an Array

--- api/shared/databaseManager.php
+++ api/shared/databaseManager.php
@@ -50,6 +50,18 @@ require_once 'abstractRestConnection.php';
 				$this->clean=false;
 			}
 		}
+		private function handleReturns($obj) : void{
+			foreach($obj as $key => $value) {
+				if(gettype($value) == 'array'){
+					$this->{$this->recoverString($key)} = "Array";
+					$this->handleReturns($obj);
+				}else{
+					if($this->recoverString($key)=="id"){$this->_id=$this->recoverString($value);}else
+					if($this->recoverString($key)=="rev"){$this->_rev=$this->recoverString($value);}else{
+					$this->{$this->recoverString($key)} = $this->recoverString($value);}
+				}
+			}
+		}
 	}
 ?>
 

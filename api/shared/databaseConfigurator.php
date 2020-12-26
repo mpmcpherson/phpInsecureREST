8b6a3803529d2b185093c07d94beb0e426d6cf7d
Michael McPherson <michael@mpmcpherson.com>
2020-12-26 03:59:46 -0500

    some more methods stubbed; config file started. Once created manually, I'll be able to start on the automation

--- api/shared/databaseConfigurator.php
+++ api/shared/databaseConfigurator.php
@@ -6,10 +6,14 @@ require_once 'CouchDBResponse.php';
 require_once 'genericException.php';
 require_once 'abstractRestConnection.php';
 
-	class databaseConfigurator extends databaseManager{
+	class databaseConfigurator{
 
 		function __construct(){}
-		function getUserId(){}
+		function getUserId(){}//not actually sure I need this, but let's note down the thought process
+		function createUserDbDimension(){}//we're going to be pulling this in when the user is searching on the META objects
+		function determine(){
+			$conf = file_get_contents('../config/databaseClassification.json');//manual is bullshit, but it's a start
+		}
 	}
 
 ?>
